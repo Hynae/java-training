8f315e19da1ad8b222c1706d717d60fb6dc0d40f
Chloe <fernandez.chloe.pro@outlook.fr>
2018-10-02 09:29:12 +0200

    exercice 1

--- src/exercices/Primitives.java
+++ src/exercices/Primitives.java
@@ -4,7 +4,20 @@ public class Primitives {
 
     // TODO 1 : Change the method 'integer' to return the expected result : int 42
     public static int integer() {
-        return 0;
+        return 42;
+    }
+
+    public static String str (){
+        return "Hello";
+    }
+    public static double real(){
+        return 3.14;
+    }
+    public static Boolean bool(){
+        return false;
+      }
+    public static void nothing(){
+        
     }
 
     // TODO 2 : Create a method 'str', result expected : String "Hello"
